efe8f51117cf008698f5d893cc846972e3dd9a24
mrsluggan <ericflyger@gmail.com>
2024-04-12 16:23:46 +0200

    secret update

--- Spa/src/App.jsx
+++ Spa/src/App.jsx
@@ -4,16 +4,20 @@ import './App.css'
 import DbTestSave from './components/dbTestSave'
 import Calender from './components/Calender'
 function App() {
-  const [weekNumber, setWeekNumber] = useState(getWeekNumber(new Date()) - 1)
+  let audio = new Audio("/SecretMusic.mp3")
 
+  const [weekNumber, setWeekNumber] = useState(getWeekNumber(new Date()) - 1)
+  //Returnar veckans nummer med hjälp av en funktion
   function getWeekNumber(date) {
     const oneJan = new Date(date.getFullYear(), 0, 1);
     const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000));
     return Math.ceil((date.getDay() + numberOfDays) / 7);
   }
+  const start = () => {
+    audio.play()
+  }
 
-
-
+  //Tillåter Pagenation, ändrar veckans nummer som i sin tur uppdaterar kalendern
   const handleOnclick = (e) => {
     const id = e.currentTarget.getAttribute('id');
     if (id === "prev") {
@@ -34,6 +38,10 @@ function App() {
           <input type="date" name="" id="" />
           <Calender weekNumber={weekNumber} />
         </div>
+
+        < div >
+          <button onClick={start}>Danger, dont press</button>
+        </div >
       </div>
     </>
   )
